65e5eb570c7b717fe4d1e6591b9828f6ec60c139
5AGGER <128492674+5AGGER@users.noreply.github.com>
2023-03-29 21:26:26 +0800

    Update W25Q128.c

--- W25Q128.c
+++ W25Q128.c
@@ -4,15 +4,16 @@
 extern SPI_HandleTypeDef hspi1;	
 
 
-uint32_t read_W25Q128_ID(void)
+int32_t read_W25Q128_ID(void)
 {
   uint8_t command = 0x9F;
   uint8_t id[3];
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET); 
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET); 
   HAL_SPI_Transmit(&hspi1, &command, 1, HAL_MAX_DELAY); 
+	HAL_Delay(1);
   HAL_SPI_Receive(&hspi1, id, 3, HAL_MAX_DELAY);       
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);  
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);  
 
   uint32_t result = (id[0] << 16) | (id[1] << 8) | id[2];
   return result;
@@ -20,20 +21,26 @@ uint32_t read_W25Q128_ID(void)
 }
 
 //next
-void W25Q128_WaitForWriteEnd(SPI_HandleTypeDef *hspi) {
+
+void W25Q128_Init(void) 
+	{
+     HAL_Delay(100);//
+  }
+
+void W25Q128_WaitForWriteEnd(SPI_HandleTypeDef *hspi1) {
   uint8_t status = 0;
   uint8_t cmd[] = {W25Q128_CMD_READ_STATUS_REG1};
 
   do {
-    HAL_SPI_Transmit(hspi, cmd, 1, HAL_MAX_DELAY);
-    HAL_SPI_Receive(hspi, &status, 1, HAL_MAX_DELAY);
+    HAL_SPI_Transmit(hspi1, cmd, 1, HAL_MAX_DELAY);
+    HAL_SPI_Receive(hspi1, &status, 1, HAL_MAX_DELAY);
   } while (status & W25Q128_WIP_FLAG);
 }
-  HAL_StatusTypeDef W25Q128_EraseSector(SPI_HandleTypeDef *hspi, uint32_t address) {
+  HAL_StatusTypeDef W25Q128_EraseSector(SPI_HandleTypeDef *hspi1, uint32_t address) {
   uint8_t cmd[4] = {W25Q128_CMD_WRITE_ENABLE};
   HAL_StatusTypeDef status;
 
-  status = HAL_SPI_Transmit(hspi, cmd, 1, HAL_MAX_DELAY);
+  status = HAL_SPI_Transmit(hspi1, cmd, 1, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
 
   cmd[0] = W25Q128_CMD_SECTOR_ERASE;
@@ -41,40 +48,51 @@ void W25Q128_WaitForWriteEnd(SPI_HandleTypeDef *hspi) {
   cmd[2] = (address >> 8) & 0xFF;
   cmd[3] = address & 0xFF;
 
-  status = HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
+  status = HAL_SPI_Transmit(hspi1, cmd, 4, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
 
-  W25Q128_WaitForWriteEnd(hspi);
+  W25Q128_WaitForWriteEnd(hspi1);
+  
   return HAL_OK;
 }
-	HAL_StatusTypeDef W25Q128_WritePage(SPI_HandleTypeDef *hspi, uint8_t *buffer, uint32_t address, uint16_t size) {
+	HAL_StatusTypeDef W25Q128_WritePage(SPI_HandleTypeDef *hspi1, uint8_t *buffer, uint32_t address, uint16_t size) {
   uint8_t cmd[4] = {W25Q128_CMD_WRITE_ENABLE};
   HAL_StatusTypeDef status;
 
-  status = HAL_SPI_Transmit(hspi, cmd, 1, HAL_MAX_DELAY);
+  status = HAL_SPI_Transmit(hspi1, cmd, 1, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
 
   cmd[0] = W25Q128_CMD_PAGE_PROGRAM;
   cmd[1] = (address >> 16) & 0xFF;
   cmd[2] = (address >> 8) & 0xFF;
   cmd[3] = address & 0xFF;
-
-  status = HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
+	
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+  
+	status = HAL_SPI_Transmit(hspi1, cmd, 4, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
 
-  status = HAL_SPI_Transmit(hspi, buffer, size, HAL_MAX_DELAY);
+  status = HAL_SPI_Transmit(hspi1, buffer, size, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
-
-  W25Q128_WaitForWriteEnd(hspi);
-  return HAL_OK;
+	
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+  
+	W25Q128_WaitForWriteEnd(hspi1);
+  
+	return HAL_OK;
 }
-	HAL_StatusTypeDef W25Q128_ReadPage(SPI_HandleTypeDef *hspi, uint8_t *buffer, uint32_t address, uint16_t size) {
+	HAL_StatusTypeDef W25Q128_ReadPage(SPI_HandleTypeDef *hspi1, uint8_t *buffer, uint32_t address, uint16_t size) {
   uint8_t cmd[4] = {W25Q128_CMD_READ_DATA, (address >> 16) & 0xFF, (address >> 8) & 0xFF, address & 0xFF};
   HAL_StatusTypeDef status;
-
-  status = HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
+	
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+  
+	status = HAL_SPI_Transmit(hspi1, cmd, 4, HAL_MAX_DELAY);
   if (status != HAL_OK) return status;
 
-  status = HAL_SPI_Receive(hspi, buffer, size, HAL_MAX_DELAY);
-  return status;
+  status = HAL_SPI_Receive(hspi1, buffer, size, HAL_MAX_DELAY);
+	
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+  
+	return status;
 }
